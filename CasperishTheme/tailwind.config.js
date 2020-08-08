6fc7a3b923f7385ceb0151c5960ae53e62d158cf
Arnaud Joubay <jub@sowenga.net>
2020-08-08 14:34:15 +0200

    Publish deploy 2020-08-08 14:34

---
+++ CasperishTheme/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+        colors: {
+          'casper-blue': '#3eb0ef',
+        }
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
